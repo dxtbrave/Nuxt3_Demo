4f28be345a192c3663903b5d2a197e233bff4c53
dxtBrave <1546758332@qq.com>
2024-02-22 23:58:03 +0800

    头部标题栏完善

--- components/defaultLayoutHeader/index.vue
+++ components/defaultLayoutHeader/index.vue
@@ -15,6 +15,10 @@
               <div class="first-title-box">
                 <a target="_self" class="first-title">{{ item.title }}</a>
               </div>
+              <!-- 展开选项卡 -->
+              <div class="hiden-box">
+                <div class="tip-box"></div>
+              </div>
             </div>
           </li>
         </ul>
@@ -26,7 +30,14 @@
 import "./index.less";
 export default {
   setup() {
-    const headerNavList = ref([{ title: "产品", id: 1 }]);
+    const headerNavList = ref([
+      { title: "产品", id: 1 },
+      { title: "解决方案", id: 2 },
+      { title: "成功故事", id: 3 },
+      { title: "新闻中心", id: 4 },
+      { title: "投资者关系", id: 5 },
+      { title: "关于拓维信息", id: 60 },
+    ]);
     return { headerNavList };
   },
 };
